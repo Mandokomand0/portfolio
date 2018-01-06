da7a7343cdf9c49c776c23afcdc56219e462fa35
Fu Ck U <d.foxultra@gmail.com>
2018-01-06 16:45:19 -0600

    server.js file successful

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var routes = require("./controllers/portfolio_controller.js");
 
 var port = process.env.PORT || 3000;
 
@@ -11,8 +12,10 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/protfolio_controller.js");
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use("/", routes);
 
 app.listen(port);
+console.log(port);
